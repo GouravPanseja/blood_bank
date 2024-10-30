a9cd2e83f259259a40821580286839b89b60804e
Sneha <snehaarora68791@gmail.com>
2024-10-30 09:54:27 +0530

    c

--- routes/mongoose.js
+++ routes/mongoose.js
@@ -31,28 +31,9 @@ const adminSchema = new mongoose.Schema({
     role : String
 });
 
-// const donorInfoSchema = new mongoose.Schema({
-//     name: String,
-//     email: String,
-//     gender: String,
-//     contact: String,
-//     age: Number,
-//     dateOfBirth: Date,
-//     idProof: String,
-//     address: String,
-//     city: String,
-//     district: String,
-//     state: String,
-//     pincode: String,
-//     bloodGroup: String,
-//     donationHistory: String,
-//     medicalHistory: String,
-//     surgeryHistory: String,
-//     diseases: String,
-// })
+
 const User = mongoose.model('User ', userSchema);
 const Donor = mongoose.model('Donor', donorSchema);
 const Admin = mongoose.model('Admin', adminSchema);
-// const donorInfo  = mongoose.model('donorInfo', donorInfoSchema); 
 
 module.exports = { User, Donor, Admin};
