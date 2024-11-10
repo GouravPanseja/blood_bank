5bf03bfa09161be1cf45f53d6988a9d48b0de642
Sneha <snehaarora68791@gmail.com>
2024-11-10 11:04:14 +0530

    bak

--- blood bank/src/main.jsx
+++ blood bank/src/main.jsx
@@ -5,11 +5,13 @@ import { createRoot } from 'react-dom/client'
 import { News } from './News.jsx'
 import "../styles/news.css"
 import { FAQs } from './FAQs.jsx'
+import { Events } from './Events.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <News/>
+    {/* <News/> */}
     {/* <FAQs/> */}
+    {/* <Events/> */}
 
   </StrictMode>,
 )
